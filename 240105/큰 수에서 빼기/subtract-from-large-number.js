4eccbab32454958b8153a1cc60c66f5079f01aa5
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-01-05 22:07:06 +0900

    Title: 큰 수에서 빼기, Time: 298ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 240105/큰 수에서 빼기/subtract-from-large-number.js
+++ 240105/큰 수에서 빼기/subtract-from-large-number.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim();
 let [a, b] = input.split(' ').map(Number);
 
-if (a >= b) {
+if (a > b) {
     console.log(a-b);
 }
 console.log(b-a);
