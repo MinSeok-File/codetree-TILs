c355756f77cd1a228c8a0580f05574bcd78f276e
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-01-08 13:53:34 +0900

    Title: 삼항연산자 2, Time: 221ms, Memory: 13MB, Status: Passed - CodeTree

--- 240108/삼항연산자 2/ternary-operator-2.js
+++ 240108/삼항연산자 2/ternary-operator-2.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
-let a = fs.readFileSync(0).toString().trim();
+let input = fs.readFileSync(0).toString().trim();
+let a = Number(input);
 let result = a == 1 ? 't' : 'f';
 
 console.log(result);
