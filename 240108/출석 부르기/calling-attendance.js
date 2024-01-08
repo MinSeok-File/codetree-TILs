7d4bf8a44c17ce55bc8a0dfbf326307307dff9db
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-01-08 14:05:47 +0900

    Title: 출석 부르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240108/출석 부르기/calling-attendance.js
+++ 240108/출석 부르기/calling-attendance.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
 let a = Number(fs.readFileSync(0).toString().trim());
 
-if (a == 1){
+if (a === 1){
     console.log('John');
 }
-else if (a == 2){
+else if (a === 2){
     consoel.log('Tom');
 }
-else if (a == 3){
+else if (a === 3){
     console.log('Paul');
 }
 else {
