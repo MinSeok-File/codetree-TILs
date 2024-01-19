5631dc1da76c81a504e09b40358b5d11a4451ea3
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-01-19 21:25:30 +0900

    Title: 세 정수의 최솟값, Time: 202ms, Memory: 16MB, Status: Passed - CodeTree

--- 240119/세 정수의 최솟값/minimum-of-three-numbers.js
+++ 240119/세 정수의 최솟값/minimum-of-three-numbers.js
@@ -4,13 +4,10 @@ let input = fs.readFileSync(0).toString().trim().split(' ');
 let a = Number(input[0]);
 let b = Number(input[1]);
 let c = Number(input[2]);
+let minN;
 
-if (a <= b && a <= c){
-    console.log(a);
-}
-else if (b <= a && b <= c){
-    console.log(b);
-}
-else if (c <= a && c <= b){
-    console.log(c);
-}
+if (a <= b && a <= c) minN = a;
+else if (b <= a && b <= c) minN = b;
+else minN = c;
+
+console.log(minN);
