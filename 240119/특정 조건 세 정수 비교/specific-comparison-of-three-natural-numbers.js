66b7a294ce0d1aca93a2dd25b8388fc50b494376
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-01-19 21:20:21 +0900

    Title: 특정 조건 세 정수 비교, Time: 237ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240119/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(' ');
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+let resA, resB;
+
+if (a <= b && a <= c){
+    resA = 1
+}
+else {
+    resA = 0
+}
+
+if (a == b && a == c){
+    resB = 1
+}
+else {
+    resB = 0
+}
+console.log(resA, resB);
