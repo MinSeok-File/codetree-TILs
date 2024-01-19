ce68aff0af9e6b1c0b8cdb52b25f471abe3c31fb
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-01-19 21:49:13 +0900

    Title: 숫자의 계절은, Time: 192ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240119/숫자의 계절은/season-of-num.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+let a = Number(input);
+
+if (a >= 3 && a <= 5) console.log('Spring');
+else if (a >= 6 && a <= 8) console.log('Summer');
+else if (a >= 9 && a <= 11) console.log('Fall');
+else console.log('Winter');
