087cd59e37d8666afae028b97d4afeb57dfc36ed
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-01-19 22:00:59 +0900

    Title: 좀 더 어려운 수학 점수, Time: 198ms, Memory: 14MB, Status: Passed - CodeTree

--- 240119/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
+++ 240119/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split('\n');
-let arrA = input[0];
-let arrB = input[1];
+let arrA = input[0].split(' ');
+let arrB = input[1].split(' ');
 
 let mathA = Number(arrA[0]);
 let engA = Number(arrA[1]);
