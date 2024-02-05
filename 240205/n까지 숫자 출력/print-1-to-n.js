ff0a48075bb57428c4433eebedd263bf8c7f729a
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-02-05 00:05:07 -0800

    Title: n까지 숫자 출력, Time: 225ms, Memory: 14MB, Status: Passed - CodeTree

--- 240205/n까지 숫자 출력/print-1-to-n.js
+++ 240205/n까지 숫자 출력/print-1-to-n.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 let n = Number(fs.readFileSync(0).toString().trim());
 
 let result = '';
-let i = 1
-while (i <= 10){
+let i = 1;
+while (i <= n){
     result += i + ' ';
     i++;
 }
