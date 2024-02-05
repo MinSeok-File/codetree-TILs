0c9e66fb318158736abd23f4f5eb1333096900e3
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-02-05 00:45:31 -0800

    Title: 높은 수에서 낮은 수까지, Time: 173ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240205/높은 수에서 낮은 수까지/from-high-to-low.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+if (a < b){
+    a, b = b, a;
+}
+
+let result = '';
+for (let i = a; i >= b; i--){
+    result += i + ' ';
+}
+
+console.log(result);
