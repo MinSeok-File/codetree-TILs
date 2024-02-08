814a827f8c517c5b4e3c0bc7a042262120a06106
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-02-08 17:11:17 +0900

    Title: 특정 조건 배수의 합, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240208/특정 조건 배수의 합/sum-of-specific-condition-multiple.js
+++ 240208/특정 조건 배수의 합/sum-of-specific-condition-multiple.js
@@ -4,9 +4,12 @@ let a = Number(input[0]);
 let b = Number(input[1]);
 let sumVal = 0;
 
-for (let i = a; i <= b; i++){
+if (a > b)[a, b] = [b, a];
+else{
+    for (let i = a; i <= b; i++){
     if (i % 5 === 0){
         sumVal += i;
     }
+}
 }
 console.log(sumVal);
