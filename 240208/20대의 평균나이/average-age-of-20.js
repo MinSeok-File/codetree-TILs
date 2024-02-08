e95a67d6399f557148022164feae97c0ebb78fd2
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-02-08 21:39:43 +0900

    Title: 20대의 평균나이, Time: 219ms, Memory: 14MB, Status: Passed - CodeTree

--- 240208/20대의 평균나이/average-age-of-20.js
+++ 240208/20대의 평균나이/average-age-of-20.js
@@ -1,12 +1,20 @@
-const fs = require('fs');
-let input = fs.readFileSync(0).toString().trim().split('\n');
-let idx = 0, sumVal = 0, cnt = 0;
+// 변수 선언 및 입력
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
 
-while(true){
-    let x = Number(input[idx]);
-    if (parseInt(x / 10) !== 2) break;
-    sumVal += x;
-    idx++;
-    cnt ++;
+let sumVal = 0, cnt = 0;
+
+// 언제 끝날지 모르므로
+// 계속 반복합니다.
+while (true) {
+    // 입력을 받습니다.
+    let n = Number(input[cnt]);
+    
+    if (parseInt(n / 10) !== 2) {
+        console.log((sumVal / cnt).toFixed(2));
+        break;
+    }
+    
+    sumVal += n;
+    cnt++;
 }
-console.log((sumVal / cnt).toFixed(2));
