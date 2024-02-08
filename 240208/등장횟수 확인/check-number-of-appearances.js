12f129b2d82d30a51cbc6e6b64a16d1df034610a
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-02-08 16:53:54 +0900

    Title: 등장횟수 확인, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240208/등장횟수 확인/check-number-of-appearances.js
+++ 240208/등장횟수 확인/check-number-of-appearances.js
@@ -3,7 +3,8 @@ let input = fs.readFileSync(0).toString().trim().split("\n");
 
 let cnt = 0;
 for (let i = 0; i < 4; i++){
-    if (i % 2 ===0){
+    let x = Number(input[i]);    
+    if (x % 2 === 0){
         cnt++;
     }
 }
