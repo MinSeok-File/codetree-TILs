fe1af39d851b5e8fb92618bbb5a6c6060f070b88
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-02-08 18:03:24 +0900

    Title: 1까지 나누기, Time: 193ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240208/1까지 나누기/divide-by-1.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+let cnt = 0;
+
+for (let i = 1; i <=1000; i++){
+    n = parseInt(n / i);
+    cnt++;
+    if (n <= 1){
+        console.log(cnt);
+        break;
+    }
+}
