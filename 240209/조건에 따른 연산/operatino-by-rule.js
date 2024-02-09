eb63d59bd634e11ae6dbfc9b7aa2ab8594d7fde1
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-02-09 19:07:06 +0900

    Title: 조건에 따른 연산, Time: 195ms, Memory: 14MB, Status: Passed - CodeTree

--- 240209/조건에 따른 연산/operatino-by-rule.js
+++ 240209/조건에 따른 연산/operatino-by-rule.js
@@ -6,11 +6,10 @@ while(true){
     if (n >= 1000) break;
     else if (n % 2 === 0){
         n = 3 * n + 1;
-        cnt++;
     }
     else if (n % 2 === 1){
         n = 2 * n + 2;
-        cnt++;
     }
+    cnt++;
 }
 console.log(cnt);
