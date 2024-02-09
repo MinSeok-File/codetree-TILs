6e44336e793d48141fda443a011e236e77555310
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-02-09 19:10:50 +0900

    Title: ab 사이에 있는 c, Time: 185ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240209/ab 사이에 있는 c/c-between-a-and-b.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+let ans = 'NO';
+
+for (let i = a; i <= b; i++){
+    if (i % c === 0) ans = 'YES'
+}
+console.log(ans);
