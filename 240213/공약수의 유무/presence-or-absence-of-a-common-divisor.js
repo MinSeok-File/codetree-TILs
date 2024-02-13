d9cfc226b0001efc29d6def6bc29d03e56ee8d73
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-02-13 10:44:31 +0900

    Title: 공약수의 유무, Time: 177ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240213/공약수의 유무/presence-or-absence-of-a-common-divisor.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+let b = Number(input[1]);
+let ans = 0;
+
+for (let i = a; i <= b; i++){
+    if (1920 % i === 0 || 2880 % i === 0){
+        ans = 1
+    }
+}
+console.log(ans);
