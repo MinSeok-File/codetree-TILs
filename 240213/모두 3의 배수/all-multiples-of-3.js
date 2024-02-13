8b7804475ac92ce6f625b2121af58a82bbafa5b8
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-02-13 10:54:19 +0900

    Title: 모두 3의 배수, Time: 196ms, Memory: 14MB, Status: Passed - CodeTree

--- 240213/모두 3의 배수/all-multiples-of-3.js
+++ 240213/모두 3의 배수/all-multiples-of-3.js
@@ -1,12 +1,20 @@
-const fs = require('fs');
-let input = fs.readFileSync(0).toString().trim().split('\n');
-let ans = 1;
+// 변수 선언 및 입력
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+let satisfied = true;
 
-for (let i = 0; i < 5; i++){
-    let x = Number(input[i]);
-    if (x % 3 !== 0){
-        ans = 0;
-        break;
+for (let i = 0; i < 5; i++) {
+    // 모든 수가 3의 배수인지 확인합니다.
+    let number = Number(input[i]);
+    if (number % 3 !== 0) {
+        satisfied = false;
     }
 }
-console.log(ans);
+
+// 출력
+if (satisfied === true) {
+    console.log(1);
+}
+else {
+    console.log(0);
+}
