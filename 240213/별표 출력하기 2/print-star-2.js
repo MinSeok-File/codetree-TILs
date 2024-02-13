da102715d922f9a8dcc2d3dd84d46bb10ebdf4de
MinSeok-File <122589906+MinSeok-File@users.noreply.github.com>
2024-02-13 11:50:20 +0900

    Title: 별표 출력하기 2, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

--- 240213/별표 출력하기 2/print-star-2.js
+++ 240213/별표 출력하기 2/print-star-2.js
@@ -5,7 +5,7 @@ let n = Number(input[0]);
 let str = '';
 for (let i = 0; i < n; i++){
     str = '';
-    for (let j = 5; j > i; j--){
+    for (let j = n; j > i; j--){
         str += '* ';
     }
     console.log(str);
